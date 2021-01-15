bbd92b1fceb1c0a5e35d38d9553ebb6d9eacb24e
Nasa28 <kaluchinasaagu@gmail.com>
2021-01-15 20:31:25 +0100

    update game_logic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -63,6 +63,13 @@ class Game
     end
   end
 
+def wining(symbol)
+  check_winning = [[0,1,2], [3,4,5], [6,7,8], [0, 4, 8], [2,4,6],[0,3,6],[1,4,7], [2,5,8]]
+  check_winning.each { |arr| return true if arr[0] = arr[1] && arr[1] = arr[2]}
+  end
+  
+
+
   def getDraws
     @draws
   end
