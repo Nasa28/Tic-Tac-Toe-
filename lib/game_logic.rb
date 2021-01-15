bdb3dd7e1cf9a6dd4168bd3d56e94e6824406687
Mohammed <atefhamosa@gmail.com>
2021-01-15 18:00:16 +0200

    Fix linters

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,21 +1,19 @@
 # Class of Player
-
 class Player
   attr_reader :name, :symbol
-  
 
   def initialize(name, symbol)
-      @name   = name
-      @symbol = symbol
-      @winTimes = 0
+    @name = name
+    @symbol = symbol
+    @win_times = 0
   end
 
-  def getWinnings
-    @winTimes
+  def winnings
+    @win_times
   end
 
-  def setWinnings
-    @winTimes +=1
+  def set_winnings
+    @win_times += 1
   end
 end
 
@@ -23,19 +21,19 @@ end
 class Board
   attr_accessor :board
   def initialize
-      @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  def updateBoard(number, symbol)
-      @board[number-1] = symbol
+  def update_board(number, symbol)
+    @board[number - 1] = symbol
   end
 
-  def displayBoard
-      puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
-      puts "---------"
-      puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
-      puts "---------"
-      puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+  def display_board
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts '---------'
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts '---------'
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
 end
 
@@ -46,39 +44,25 @@ class Game
     @draws = 0
   end
 
-  def checkWin(board, player)
-    if(board[0] == board[1] && board[1] == board[2] || board[3] == board[4] && board[4] == board[5] || board[6] == board[7] && board[7]== board[8])
-      player.setWinnings
-      return player
-    elsif(board[0] == board[3] && board[3] == board[6] || board[1] == board[4] && board[4] == board[7] || board[2] == board[5] && board[5]== board[8])
-      player.setWinnings
-      return player
-    elsif(board[0] == board[4] && board[4] == board[8] || board[2] == board[4] && board[4] == board[6])
-      player.setWinnings
-      return player
+  def check_win(board, player)
+    if board[0] == board[1] && board[1] == board[2] || board[3] == board[4] && board[4] == board[5] || board[6] == board[7] && board[7] == board[8]
+      player.set_winnings
+      player
+    elsif board[0] == board[3] && board[3] == board[6] || board[1] == board[4] && board[4] == board[7] || board[2] == board[5] && board[5] == board[8]
+      player.set_winnings
+      player
+    elsif board[0] == board[4] && board[4] == board[8] || board[2] == board[4] && board[4] == board[6]
+      player.set_winnings
+      player
     else
-      return false
+      false
     end
   end
 
-  def getDraws
-    @draws
-  end
+  # Check winner
+  def check(check_win_meth)
+    return true unless check_win_meth == true
 
-  def setDraws
-    @draws +=1
+    puts "The winner is #{check_win_meth.name}" unless check_win_meth == true
   end
-
-# Check winner
-  def check(gameCheckWinMethod)
-    if gameCheckWinMethod != false
-      puts "The winner is #{gameCheckWinMethod.name}"
-      return true
-    end
-  end
-
 end
-
-
-
- 
